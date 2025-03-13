7f8b6a75e8df55190ae19497c0a261690f4f0bfa
Tharindu Senadheera <tharindu.s@euka.edu.au>
2025-03-13 22:18:37 +0530

    fixes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+    content: [
+      './resources/**/*.blade.php',
+      './resources/**/*.js',
+      './resources/**/*.jsx',
+      './resources/**/*.ts',
+      './resources/**/*.tsx',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
